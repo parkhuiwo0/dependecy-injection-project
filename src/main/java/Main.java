9f67c2e5a6b0193d0f23210240310879fdb5da0b
HuiwooPark <parkhuiwo0@gmail.com>
2020-08-09 17:52:22 +0900

    init project

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Huiwoo Park
+ * User: common
+ * Date: 2020/08/09
+ * Time: 5:51 오후
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
